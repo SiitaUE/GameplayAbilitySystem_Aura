1f7e586633a2f2d87cc25f05806b5f1ab5f3ca6c
siita rio <cercleneptune@gmail.com>
2025-01-16 12:32:35 +0100

    Input Config Data Asset

---
+++ Source/Aura/Private/Input/AuraInputConfig.cpp
@@ -0,0 +1,22 @@
+// Copyright DambrainePL
+
+
+#include "Input/AuraInputConfig.h"
+
+const UInputAction* UAuraInputConfig::FindAbilityInputAction(const FGameplayTag& InputTag, bool bLogNotFound) const
+{
+	for (const FAuraInputAction& Action: AbilityInputActions)
+	{
+		if (Action.InputAction && Action.InputTag == InputTag)
+		{
+			return Action.InputAction;
+		}
+	}
+
+	if (bLogNotFound)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Can't Find AbilityInputAction for InputTag [%s], on InputConfig [%s]"), *InputTag.ToString(), *GetNameSafe(this))
+	}
+
+	return nullptr;
+}
