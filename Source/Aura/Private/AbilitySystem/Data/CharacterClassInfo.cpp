8f7d0c024dcfd39b660a708d8f5bf6e7dd87f1dd
siita rio <cercleneptune@gmail.com>
2025-02-01 16:43:00 +0100

    Curve Tables

---
+++ Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,9 @@
+// Copyright DambrainePL
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass) const
+{
+	return CharacterClassInformation.FindChecked(CharacterClass);
+}
